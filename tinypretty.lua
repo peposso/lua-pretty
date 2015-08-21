c5122d72077a434a4c9a476c571e236a7566a875
岡田 悠甫 <yusuke.okada@gu3.co.jp>
2015-08-21 19:33:07 +0900

    shorten

--- tinypretty.lua
+++ tinypretty.lua
@@ -3,29 +3,11 @@ return function(...)
   local arg = select('#', ...) > 1 and {...} or ...
   local cycle = {}
   local function pretty(t)
-    if type(t) == 'string' then return ('%q'):format(t) end
     if type(t) ~= 'table' then return tostring(t) end
     if cycle[t] then return '{*cycle}' end
     cycle[t] = true
-    local ls, keys, index = {}, {}, 1
-    for k in pairs(t) do keys[#keys+1] = k end
-    table.sort(keys, function(a, b)
-      local ok, ret = pcall(function() return a < b end)
-      if ok then return ret end
-      local ta, tb = type(a), type(b)
-      if ta ~= tb then return ta < tb end
-      return tostring(a) < tostring(b)
-    end)
-    for i = 1, #keys do
-      local k = keys[i]
-      if k == index then
-        ls[#ls+1] = pretty(t[k])
-        index = index + 1
-      elseif type(k) == 'string' and k:match('^[%l%u_][%w_]*$') then
-        ls[#ls+1] = ('%s=%s'):format(k, pretty(t[k]))
-      else
-        ls[#ls+1] = ('[%s]=%s'):format(pretty(k), pretty(t[k]))
-      end
+    local ls = {}; for k, v in pairs(t) do
+      ls[#ls+1] = ('%s=%s'):format(k, v)
     end
     cycle[t] = nil
     return '{'..table.concat(ls, ',')..'}'

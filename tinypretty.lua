0932fe3e341b8e88944925d6557a93fdbce2b617
岡田 悠甫 <yusuke.okada@gu3.co.jp>
2015-09-18 13:53:06 +0900

    update tinypretty

--- tinypretty.lua
+++ tinypretty.lua
@@ -2,12 +2,23 @@ local print = print
 return function(...)
   local arg = select('#', ...) > 1 and {...} or ...
   local cycle = {}
+  local tinsert = table.insert
   local function pretty(t)
     if type(t) ~= 'table' then return tostring(t) end
     if cycle[t] then return '{*cycle}' end
     cycle[t] = true
-    local ls = {}; for k, v in pairs(t) do
-      ls[#ls+1] = ('%s=%s'):format(k, v)
+    local ls = {}
+    if t[1] ~= nil then
+      for i = 1, #t do tinsert(ls, pretty(t[i])) end
+      for k, v in pairs(t) do
+        if type(k) ~= 'number' or math.floor(k) ~= k or k <= 0 or #t < k then
+          tinsert(ls, ('%s=%s'):format(k, pretty(v)))
+        end
+      end
+    else
+      for k, v in pairs(t) do
+        tinsert(ls, ('%s=%s'):format(k, pretty(v)))
+      end
     end
     cycle[t] = nil
     return '{'..table.concat(ls, ',')..'}'
